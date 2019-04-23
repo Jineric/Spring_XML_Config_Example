02fbe942648fd08c6bee861d5498262679ce47e0
Eric Baek <eeb0129@ATL42EEB071221.local>
2019-04-23 10:24:01 -0400

    finished setter injection

--- src/main/java/com/jineric/Spring_XLM_Config_Example/SpringBeanExampleApplication.java
+++ src/main/java/com/jineric/Spring_XLM_Config_Example/SpringBeanExampleApplication.java
@@ -11,9 +11,22 @@ public class SpringBeanExampleApplication {
 //        Coach theCoach = new TrackCoach();
 //
 //        System.out.println(theCoach.getDailyWorkout());
+
+        /* construction injection
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        System.out.println(theCoach.getDailyWorkout());
+
+        System.out.println(theCoach.getDailyFortune());
+
+        context.close();
+        */
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
         System.out.println(theCoach.getDailyWorkout());
 
